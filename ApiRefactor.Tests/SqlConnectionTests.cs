f6ca16197b82499c1400f4ab4ff58e5c79cf6e6e
ClocoDevelopmentGit <development@cloco.com.au>
2025-11-30 16:45:19 +1100

    Included unit tests before refactor

---
+++ ApiRefactor.Tests/SqlConnectionTests.cs
@@ -0,0 +1,51 @@
+﻿using ApiRefactor.Models;
+using Microsoft.Data.Sqlite;
+using Shouldly;
+
+namespace ApiRefactor.Tests;
+
+public class SqlConnectionTests
+{
+    [Fact]
+    public void GetSqlConnection_ShouldReturnSqliteConnection()
+    {
+        // Act
+        var connection = SqlConnection.GetSqlConnection();
+
+        // Assert
+        connection.ShouldNotBeNull();
+        connection.ShouldBeOfType<SqliteConnection>();
+    }
+
+    [Fact]
+    public void GetSqlConnection_ShouldHaveCorrectConnectionString()
+    {
+        // Act
+        var connection = SqlConnection.GetSqlConnection();
+
+        // Assert
+        connection.ConnectionString.ShouldContain("waves.db");
+        connection.ConnectionString.ShouldContain("App_Data");
+    }
+
+    [Fact]
+    public void GetSqlConnection_ShouldReturnNewInstanceEachTime()
+    {
+        // Act
+        var connection1 = SqlConnection.GetSqlConnection();
+        var connection2 = SqlConnection.GetSqlConnection();
+
+        // Assert
+        connection1.ShouldNotBeSameAs(connection2);
+    }
+
+    [Fact]
+    public void GetSqlConnection_ShouldReturnConnectionInClosedState()
+    {
+        // Act
+        var connection = SqlConnection.GetSqlConnection();
+
+        // Assert
+        connection.State.ShouldBe(System.Data.ConnectionState.Closed);
+    }
+}
